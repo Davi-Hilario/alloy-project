44e38374bb418d4d610a6c80a317dd55e76fbcac
Davi-Hilario <davihilario23@gmail.com>
2024-04-30 15:02:14 -0300

    feat: adicionando mensagem de carrinho vazio

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -4,14 +4,45 @@ import { toggleAddToCart } from "../../redux/slices/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../../components/navbar/Navbar";
 import ProductCard from "../../components/cards/productCard/ProductCard";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import doesTokenExists from "../../validateAuthentication";
+import Button from "../../components/buttons/Buttons";
 
 function Cart() {
     const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.cart)
 
+    let [isLogged, setLogged] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!doesTokenExists()) {
+            sendToLoginPage();
+        } else {
+            setLogged(doesTokenExists());
+        }
+    }, [])
+
+    function sendToLoginPage() {
+        alert("You must log-in before adding products to your cart!")
+        navigate("/login")
+    }
+
     return (
         <div className={style["Cart"]}>
             <Navbar />
+            { cartItems.length === 0 && 
+                <div className={style["message-area"]}>
+                    <span>Your cart has no products yet!</span>
+                    <Button 
+                        onClick={()=> navigate("/")} 
+                        value="Go Shopping!" 
+                        width="30%" 
+                        height="4rem" 
+                    />
+                </div>
+            }
             { cartItems.length !== 0 &&
                 <div className={style["found-items-area"]}>
                     { cartItems && cartItems.map((data, index) => (
